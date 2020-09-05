9717257546d7d4633ba578a17629333523d64eef
Adenilson Pantoja Moura <69207@PROWAY.TREINA>
2020-09-05 16:25:32 -0300

    2-Palavras reservadas

--- App_Java/src/br/com/fundamentos/Escape.java
+++ App_Java/src/br/com/fundamentos/Escape.java
@@ -2,6 +2,10 @@ package br.com.fundamentos;
 
 public class Escape {
 	public static void main(String[] args) {
+		//Aqui é um comentario
+		/*
+		 Comentarios de Multiplas Linhas
+		 */
 		System.out.println("Usando o \b Eclipese");
 		System.out.println("Usando o \t Eclipse");
 		System.out.println("Usando o \" Eclipse\"");
---
+++ App_Java/src/br/com/fundamentos/Palavras_reservadas.java
@@ -0,0 +1,9 @@
+package br.com.fundamentos;
+
+public class Palavras_reservadas {
+
+	public Palavras_reservadas() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
