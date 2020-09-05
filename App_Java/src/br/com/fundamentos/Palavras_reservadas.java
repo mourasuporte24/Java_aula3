cc46a1d165dd823bb3a93dae5e2ce49dd0e1c33a
Adenilson Pantoja Moura <69207@PROWAY.TREINA>
2020-09-05 16:41:41 -0300

    3 - Trabalhando com Variaveis

--- App_Java/src/br/com/fundamentos/Palavras_reservadas.java
+++ App_Java/src/br/com/fundamentos/Palavras_reservadas.java
@@ -3,7 +3,11 @@ package br.com.fundamentos;
 public class Palavras_reservadas {
 
 	public Palavras_reservadas() {
-		// TODO Auto-generated constructor stub
+		/*
+		 *Palavras reservadas são palavras que a linguagem utiliza
+		 * ex: int,string,void		 
+		 * http://www.linhadecodigo.com.br/artigo/83/as-52-palavras-reservadas-do-java.aspx
+		 */
 	}
 
 }
---
+++ App_Java/src/br/com/fundamentos/TrabalhandoComVariaveis.java
@@ -0,0 +1,18 @@
+package br.com.fundamentos;
+
+
+public class TrabalhandoComVariaveis {
+
+	
+		public static void main(String[] args) {
+			int quantidade; //Declarando variavel inteiro
+			quantidade =10; //Atribuindo o valor 10
+			System.out.println(quantidade);	
+			quantidade=10;
+			System.out.println(quantidade);
+			quantidade=15;
+		
+		
+	}
+
+}
