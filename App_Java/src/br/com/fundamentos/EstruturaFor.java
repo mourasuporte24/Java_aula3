fc82533d08e500b13c30b93fbab16f44e3269573
Adenilson Pantoja Moura <69207@PROWAY.TREINA>
2020-09-20 17:44:35 -0300

    Metodo For

---
+++ App_Java/src/br/com/fundamentos/EstruturaFor.java
@@ -0,0 +1,17 @@
+package br.com.fundamentos;
+
+import java.util.Scanner;
+
+public class EstruturaFor {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Ultimo numero: ");
+		int numeroFinal = entrada.nextInt();
+		
+		for(int i =1; i <=10; i++);
+		
+		
+	}
+
+}
